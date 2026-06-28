83bb3ab3091ee455f6df2e9432110d86e6cc1592
Satria Nugraha <sakazukiindarto491@gmail.com>
2026-06-28 20:08:31 +0700

    Menambahkan pendaftaran_kelas ke dalam dashboard > Manage Class / Kelas

--- src/View/Dashboard_Utama.java
+++ src/View/Dashboard_Utama.java
@@ -155,6 +155,7 @@ public class Dashboard_Utama extends javax.swing.JFrame {
         jMenu4 = new javax.swing.JMenu();
         jMenu5 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -358,6 +359,10 @@ public class Dashboard_Utama extends javax.swing.JFrame {
         jMenuItem2.addActionListener(this::jMenuItem2ActionPerformed);
         jMenu5.add(jMenuItem2);
 
+        jMenuItem3.setText("Pendaftaran Kelas");
+        jMenuItem3.addActionListener(this::jMenuItem3ActionPerformed);
+        jMenu5.add(jMenuItem3);
+
         jMenuBar1.add(jMenu5);
 
         setJMenuBar(jMenuBar1);
@@ -438,6 +443,12 @@ public class Dashboard_Utama extends javax.swing.JFrame {
         dispose();
     }//GEN-LAST:event_jMenu1ActionPerformed
 
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+        new pendaftaran_kelas().setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -471,6 +482,7 @@ public class Dashboard_Utama extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel5;
